567e32d7484f05c956f8fa9850cf4a38b48d7cd3
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-25 20:16:42 -0500

    Fixed issue with https://gist.github.com/mikeschinkel/5028467 plus some commenting.

--- typed-config.php
+++ typed-config.php
@@ -1,12 +1,23 @@
 <?php
 /**
- * @package Typed Config Lib for PHP
+ * Loads JSON into defined classes to enable default values, data cleansing, validation, etc.
  *
- * @version 0.1.0
+ * PHP version 5.3
  *
- * Enables loading of JSON files into defined classes to enable default values and validation.
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
  *
+ * @package   Typed Config
+ * @author    Mike Schinkel <mike@newclarity.net>
+ * @copyright 2013 NewClarity LLC
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html
+ * @version   0.2.0
+ * @link      http://github.com/newclarity.net/typed-config
  */
+
 require ( __DIR__ . "/includes/class-typed-config-loader.php" );
 require ( __DIR__ . "/includes/class-typed-config.php" );
 
