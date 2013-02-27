bcd4e39ec1777fd7dfefd5ca394ed653c309d132
Mike Schinkel <mikeschinkel@gmail.com>
2013-02-27 07:33:32 -0500

    Changed to 0.3.1

--- typed-config.php
+++ typed-config.php
@@ -6,7 +6,7 @@
  * @author    Mike Schinkel <mike@newclarity.net>
  * @copyright 2013 NewClarity LLC
  * @license   http://www.gnu.org/licenses/gpl-2.0.html
- * @version   0.3.0
+ * @version   0.3.1
  * @link      http://github.com/newclarity.net/typed-config
  * @requires  PHP 5.3
  */
